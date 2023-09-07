c62a452767471cd1d071648ce894d6762e3fe891
Zarifxon <zarifsf@mail.ru>
2023-09-07 16:55:24 +0500

    1-dars

---
+++ 1-dars.py
@@ -0,0 +1,16 @@
+# input O'ziga qiymat qabul qilishini so'raydi
+# ism = input("Ismingizni kiriting:")
+# familiya = input("Familiyangizni kiriting:")
+# yosh = input("Yoshingizni kiriting:")
+# print(f"Sizning ismningiz: {ism} familiyangiz: {familiya} yoshingiz: {yosh} da")
+
+
+# a=int(input("Birinchi sonni kiriting:"))
+# b=int(input("ikkinchi sonni kiriting:"))
+# c=a+b
+# print(f"Yig'indini chiqaramiz: {c} ")
+
+# print('Mening o\'qishim 15.00 da boshlanadi')
+
+print(int(15625**0.5))
+
