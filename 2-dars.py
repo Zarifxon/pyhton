9b6469729bb22c52e41148eae037ff52d0653000
Zarifxon <zarifsf@mail.ru>
2023-09-07 16:55:57 +0500

    Mavzu
    07.09.2023 sanada o'tilgan mavzu

---
+++ 2-dars.py
@@ -0,0 +1,58 @@
+# # print() funksiyasi ifoda matnli qiymat bo'ladimi son bo'ladimi chop ishga tushirish uchun ishlatiladi
+
+# print()
+
+# a="Hello World"
+# b=25
+# print(a)
+# print(type(a))
+# print(type(b))
+
+# ism="Davron"
+# familiya="Nizomxonov"
+# yosh=16
+# print("ism",  "familiya",  "yosh")
+# print("Ismingiz:",  ism, "familiyangiz",  familiya,  "yoshingiz:", yosh)
+# izoh="Davron Nizomxonov 2007  yilda tug'ilgan. \nHozirda Safoxo'ja qishlog'ida yashayd "
+# print("""Davron Nizomxonov 2007  yilda tug'ilgan. 
+#       Hozirda Safoxo'ja qishlog'ida yashayd.
+#       Maktabda  10 sinfda o'qiydi.
+#       Baholari doimo 5.
+#       """)
+
+# qishloq="Mening \"Spark\" mashinam bor"
+# print(qishloq)
+
+# print("Mening o'qishim 15.00 da boshlanadi")
+# print('Mening o\'qishim 15.00 da boshlanadi')
+
+
+### Arifmetik amallar
+# a=16
+# b=4
+# c=a*b #Ko'paytmani chiqaradi
+# d=a-b #Ayirmani chiqaramiz
+# g=a+b #Yig'indini
+# h=a/b #Bo'linmani
+# i=a*b  #Ko'paytma
+# j=a**b  # "a"ning "b" darajasi
+# z=a%b # Qoldiq
+# k=a//b #a ni b ga bo'lgandagi bo'linmani butun qismini chiqaradi
+# dav=a**b
+# x=64
+# y=0.5
+# print(float(h)) 
+
+# 2^2=4
+# 2^3=8
+# 2^4=16
+# 2^5=32
+# 2^6=64
+# 2^7=128
+# 2^8=256
+# 2^9=512
+
+# a=729
+# b=(1/3)
+# c=a**b
+# print(c)
