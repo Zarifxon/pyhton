fe4e8972a911b908f2c0a31952beecd6bafe494b
Zarifxon <zarifsf@mail.ru>
2023-09-07 17:01:32 +0500

    Vazifa 09.09.2023
    Vazifa 09.09.2023 da bajarilishi shart

---
+++ 2-dars_vazifa.py
@@ -0,0 +1,10 @@
+1. Maning uyim Safoxo’jada joylashgan!
+2. Mashinaning baloni ‘to’rtta’ ekanligi rostmi?
+3. Doiraning yuzini va uzunligini toping! D=2r=34 metr, *π*=3.14
+4. Yegirma beshning 3 darajasini topib Ikkinchi darajali ildizdan chiqaring 
+5. Tomonlari 162 metr ga teng kvadratning yuzi va perimetrini toping.
+6. Katetlari 6 va 7 metr bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini, yuzini peremetr va yarim peremetrini toping (Pifager teoremasidan [](https://fayllar.org/pifagor-teoremasi-va-uning-turli-hil-isbotlari-mavzusida-tajer.html)foydalaning)
+
+> Izoh. Bu Misollarni ishlang va natijada misolning faqat javobi emas matnli ifoda ham qatnashib javob chiqsin. M: Doiraning radiusi 5 ga teng bo’lsa: Yuzasi 25 ga teng bo’ladi.
+kod yozishda  (#) heshteg belgisidan foydalaning
+>
